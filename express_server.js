c60eb5f052530e5bd045ebc0390a52e336b02f6c
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-08-03 00:00:34 +0000

    post and get method added for new urls

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+
+function generateRandomString() {
+  
+}
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -17,17 +23,27 @@ app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render('urls_index', templateVars);
 }); 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+
   res.render("urls_show", templateVars);
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
 
 app.listen(PORT, () => {
